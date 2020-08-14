728764044f02bf1607a74c263584a892f8c6ae4e
DawidJS <48735427+DawidJS@users.noreply.github.com>
2020-08-14 19:47:18 +0200

    start project

---
+++ get.js
@@ -0,0 +1,10 @@
+let ajax = new XMLHttpRequest();
+ajax.open('GET', 'test.eko.eu/GET', true);
+ajax.send();
+
+function addApi() {
+    ajax.onreadystatechange = function() {
+        let divJS = document.getElementById("divHtml");
+        divJS.innerHTML = "koty";
+    }
+}
